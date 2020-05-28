678da0df8d309dd43c7844916332306fcaf0670f
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-05-28 13:38:54 -0600

    :crystal_ball: Working on connecting api and mobX

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { useEffect } from 'react';
-
 import './App.css';
 import { allStrainsData } from './apiRequest'
+import store from './WeedStore'
 
 function App() {
 
 
- useEffect(() => {
-     let fetchData = async () => {
-       let data = await allStrainsData()
-       console.log(data);
-       
-     }
-     fetchData()
-    }, [])
+//  useEffect(() => {
+//      let fetchData = async () => {
+//        let data = await allStrainsData()
+
+//      }
+//      fetchData()
+//     }, [])
   
-   
+   console.log(store)
 
   return (
     <div className="App">
