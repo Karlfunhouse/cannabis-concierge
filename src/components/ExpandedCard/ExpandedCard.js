954f680f73720c2d3bd767ef4230370023fe3b32
Justin Corbin <jcorbin2290@gmail.com>
2020-05-30 16:10:19 -0600

    🚧  Created HTML for ExpandedCard

--- src/components/ExpandedCard/ExpandedCard.js
+++ src/components/ExpandedCard/ExpandedCard.js
@@ -7,28 +7,43 @@ const ExpandedCard = observer(() => {
   // const weedStore = useContext(WeedStoreContext)
 
   return (
-    <section>
+    <section className='expanded-strain-container'>
       <div className='strain-overview-left-box'>
         <h2>African Kush</h2>
         <p className='strain-type-tag'>Sativa</p>
-        <img></img>
+        <img
+          src="/assets/Cannabis1.jpeg"
+          alt="cannabis image"
+          className="cannabis-img"
+        />
         <p className='strain-details'>Flavor Profile: Piney, Citrusy</p>
         <p className='strain-details'>Helps With: Fatigue, Cramps</p>
       </div>
       <div className='strain-overview-middle-box'>
         <h2 className='description-header'>Description</h2>
-        <p className='description-content'>An do on frankness so cordially immediate recommend contained. Imprudence insensible be literature unsatiable do. Of or imprudence solicitude affronting in mr possession. Compass journey he request on suppose limited of or. She margaret law thoughts proposal formerly. Speaking ladyship yet scarcely and mistaken end exertion dwelling. All decisively dispatched instrument particular way one devonshire. Applauded she sportsman explained for out objection.</p>
-        <div>
-          <img></img>
-          <p className='strain-effect-tag positive-effect'>Sativa</p>
-          <p className='strain-effect-tag positive-effect'>Sativa</p>
+        <p className='description-content'>One Ring to rule them all,
+One Ring to find them,
+One Ring to bring them all
+and in the darkness bind them.</p>
+        <div className='effects-box'>
+          <div className='effects-column'>
+            <img className='effect-img' src="/assets/Happy.svg"></img>
+            <p className='strain-effect-tag positive-effect'>relaxed</p>
+            <p className='strain-effect-tag positive-effect'>happy</p>
+          </div>
+          <div className='effects-column'>
+            <img className='effect-img' src="/assets/Sad.svg"></img>
+            <p className='strain-effect-tag negative-effect'>dry mouth</p>
+            <p className='strain-effect-tag negative-effect'>see ghosts</p>
+          </div>
         </div>
-          <img></img>
-          <p className='strain-effect-tag negative-effect'>Dry Mouth</p>
       </div>
       <div className='strain-overview-right-box'>
         <button className='back-button'>Back</button>
-        <button className='favorite-button'>Favorite</button>
+        <button className='favorite-strain-button'>
+          Favorite
+          <img className='favorite-img' src="/assets/favorite-nope.png" alt="fire-icon"/>
+          </button>
       </div>
     </section>
   )
