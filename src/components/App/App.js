0557015ad6cd6375692114ef66ada6bdc65d3e12
Ally Jarjour <allyjarjour@gmail.com>
2020-05-30 11:22:46 -0600

    :crystal_ball: Add routing to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useContext } from 'react';
 import './App.css';
-import { allStrainsData } from '../../apiRequest';
-import store from '../../WeedStore';
 import Login from '../Login/Login.js';
 import { Switch, Route } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar.js';
+import CategoryCards from '../CategoryCards/CategoryCards.js';
 import CardsContainer from '../CardsContainer/CardsContainer.js';
 import { WeedStoreContext } from '../../WeedStore';
 
@@ -20,13 +19,49 @@ const App = () => {
   return (
     <div className="App">
       <Switch>
-        <Route path='/home' component= { () => (
-          <div className='home-page'>
-            <NavBar />
-            <CardsContainer />
-          </div>
-        ) } />
-        <Route path='/' component= { () => <Login /> } />
+        <Route
+          path="/quiz"
+          component={() => (
+            <div>
+              <NavBar />
+            </div>
+          )}
+        />
+        <Route
+          path="/medicinal"
+          component={() => (
+            <div>
+              <NavBar />
+              <CardsContainer />
+            </div>
+          )}
+        />
+        <Route
+          path="/mood"
+          component={() => (
+            <div>
+              <NavBar />
+            </div>
+          )}
+        />
+        <Route
+          path="/activity"
+          component={() => (
+            <div>
+              <NavBar />
+            </div>
+          )}
+        />
+        <Route
+          path="/home"
+          component={() => (
+            <div className="home-page">
+              <NavBar />
+              <CategoryCards />
+            </div>
+          )}
+        />
+        <Route path="/" component={() => <Login />} />
       </Switch>
     </div>
   );
