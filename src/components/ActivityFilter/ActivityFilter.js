7e99c6c787933dd3fd0ae8aa42e7e576ef6dbf18
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-06-01 15:20:43 -0600

    :art: Styling for Activities buttons/ :crystal_ball: FUnctionality to add activities buttons

--- src/components/ActivityFilter/ActivityFilter.js
+++ src/components/ActivityFilter/ActivityFilter.js
@@ -2,24 +2,28 @@ import React, { useContext } from "react";
 import "./ActivityFilter.css";
 import { WeedStoreContext } from "../../WeedStore";
 import { observer } from "mobx-react-lite";
+import { useLocation } from "react-router-dom"
 
 const ActivityFilter = observer( () => {
     const weedStore = useContext(WeedStoreContext);
     const location = useLocation();
-    const { type } = useParams();
+   
 
+    const activityButtons = weedStore.activities.map(activity => {
+       
+        return (
+            <button className={activity.css}><span className="activity-name">{activity.title}</span></button>
+        )
+    })
+    
     const filterByActivity = (e) => {
-        console.log(type)
         
-    }
-
-    const activityCards = () => {
-        return ()
     }
 
     return (
         <div className="activity-card-container">
-
+            <h2>Choose Your Adventure</h2>
+            {activityButtons}
         </div>
     );
 })
