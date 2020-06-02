3cf33793560559c9b4d137e48dbf9ed45942adbb
Leah Winters <leah.winters24@gmail.com>
2020-06-02 17:02:50 -0400

    Wrote and passed tests for the nav bar

---
+++ src/components/NavBar/NavBar.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NavBar from './NavBar';
+// import { jest } from '@jest/globals'
+import { render, waitFor, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<BrowserRouter> <NavBar /> </BrowserRouter>, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
+describe('<NavBar />', () => {
+
+  it('User should see a title welcoming them to the application', () => {
+    const { getByText } = render(<BrowserRouter> <NavBar /> </BrowserRouter>);
+
+    expect(getByText('Welcome to Concierge')).toBeInTheDocument();
+  })
+
+  it('User should see a favorites button', () => {
+    const { getByText } = render(<BrowserRouter> <NavBar /> </BrowserRouter>);
+
+    expect(getByText('Favorites - 0')).toBeInTheDocument();
+  })
+
+  it('User should see a logout button', () => {
+    const { getByText } = render(<BrowserRouter> <NavBar /> </BrowserRouter>);
+
+    expect(getByText('Logout')).toBeInTheDocument();
+  })
+
+  // tried to test that the favs button was clicked one time, but did not work as I expected it to, left to show how I tried:
+
+  // it('Should be able to click the favorites button', () => {
+  //   const { getByPlaceholderText, getByText } = render(<BrowserRouter> <NavBar /> </BrowserRouter>);
+  //   const mockViewFavs = jest.fn();
+  //   const favoriteBtn = getByText('Favorites - 0');
+  //
+  //   fireEvent.click(favoriteBtn)
+  //   expect(mockViewFavs).toHaveBeenCalledTimes(1);
+  //
+  // })
+
+})
