302f422853a7b821380233da66c4e53dd486753a
Ally Jarjour <allyjarjour@gmail.com>
2020-06-02 16:57:28 -0600

    :crystal_ball: Add toggle title for rendering the Effects or Flavor filter

--- src/components/MedicalAndEffectsFilter/MedicalAndEffectsFilter.js
+++ src/components/MedicalAndEffectsFilter/MedicalAndEffectsFilter.js
@@ -1,16 +1,24 @@
-import React, { useEffect, useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./MedicalAndEffectsFilter.css";
 import { useLocation } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 import { WeedStoreContext } from "../../WeedStore";
+import FlavorFilter from '../FlavorFilter/FlavorFilter';
+
 
 const MedicalAndEffectsFilter = observer(() => {
   const weedStore = useContext(WeedStoreContext);
   const location = useLocation(); //returns path of page for conditional
   const locationPath = location.pathname.substr(1) // taking off forward slash
+  const [hide, addClass] = useState("null");
+
+  useEffect(() => {
+    if (locationPath === "medicinal") {
+      addClass("hide");
+    }
+}, [])
 
   weedStore.getSelectorStatus(locationPath)
-
   const allFilterClickListener = (e) => {
     weedStore.resetDesiredEffects()
     const name = e.target.name
@@ -37,12 +45,56 @@ const MedicalAndEffectsFilter = observer(() => {
     )
   );
 
+  const moodOrFlavor = (
+    <div>
+      <h3 className="filter-title">
+        Please filter by
+        <label
+          className={
+            (weedStore.filteredByEffect === null ||
+              weedStore.filteredByEffect === true) ?
+            "selected-topic" : null
+          }
+        >
+          <input
+            type="radio"
+            value="effect"
+            name="title"
+            onClick={(e) => weedStore.updateFilterByEffect(e.target.value)}
+          />{" "}
+          Effect{" "}
+        </label>
+        or
+        <label
+          className={
+            (weedStore.filteredByEffect === false ? "selected-topic" : null)
+          }
+        >
+          <input
+            type="radio"
+            value="flavor"
+            name="title"
+            onClick={(e) => weedStore.updateFilterByEffect(e.target.value)}
+          />{" "}
+          Flavor
+        </label>
+      </h3>
+    </div>
+  );
+
   return (
     <>
-      <h3 className="filter-title">{locationPath === "medicinal" ? "Filter your selection by Medical Symptoms" : "Filter your selection by effects"}</h3>
-      <div className="selection-menu">
-        {selectionMenu}
-      </div>
+      <h3 className="filter-title">
+        {locationPath === "medicinal"
+          ? "Filter your selection by Medical Symptoms"
+          : moodOrFlavor}
+      </h3>
+        {weedStore.filteredByEffect === null ||
+        weedStore.filteredByEffect === true ? (
+            <div className="selection-menu"> {selectionMenu} </div>
+        ) : (
+          <FlavorFilter className={hide} />
+        )}
     </>
   );
 })
