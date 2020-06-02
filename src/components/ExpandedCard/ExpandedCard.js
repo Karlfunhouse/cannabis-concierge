450a6c479b83e72e0d2e56bc0c87592325741a33
Justin Corbin <jcorbin2290@gmail.com>
2020-06-02 14:53:16 -0600

    Allowed for dynamic passing of effects to card

--- src/components/ExpandedCard/ExpandedCard.js
+++ src/components/ExpandedCard/ExpandedCard.js
@@ -1,40 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ExpandedCard.css'
 import { WeedStoreContext } from '../../WeedStore'
 import { observer } from 'mobx-react-lite';
 
 const ExpandedCard = observer(() => {
-  // const weedStore = useContext(WeedStoreContext)
+  const weedStore = useContext(WeedStoreContext)
+  const negEffects = weedStore.selectedStrain.effects.negative
+    .map(effect => (<p className='strain-effect-tag positive-effect'>{effect}</p>))
+
+  const posEffects = weedStore.selectedStrain.effects.positive
+    .map(effect => (<p className='strain-effect-tag positive-effect'>{effect}</p>))
 
   return (
     <section className='expanded-strain-container'>
+    {console.log(weedStore.selectedStrain)}
       <div className='strain-overview-left-box'>
-        <h2>African Kush</h2>
-        <p className='strain-type-tag'>Sativa</p>
+        <h2>{weedStore.selectedStrain.name}</h2>
+        <p className='strain-type-tag'>{weedStore.selectedStrain.race}</p>
         <img
           src="/assets/Cannabis1.jpeg"
           alt="cannabis image"
           className="cannabis-img"
         />
-        <p className='strain-details'>Flavor Profile: Piney, Citrusy</p>
-        <p className='strain-details'>Helps With: Fatigue, Cramps</p>
+        <p className='strain-details'>Flavor Profile: {weedStore.selectedStrain.flavors.join(', ')}</p>
+        <p className='strain-details'>Helps With: {weedStore.selectedStrain.effects.medical.join(', ')}</p>
       </div>
       <div className='strain-overview-middle-box'>
         <h2 className='description-header'>Description</h2>
-        <p className='description-content'>One Ring to rule them all,
-One Ring to find them,
-One Ring to bring them all
-and in the darkness bind them.</p>
+        <p className='description-content'>a sativa-dominant hybrid originating in California, has achieved legendary status among West Coast strains. Crossing Blueberry with Haze, Blue Dream balances full-body relaxation with gentle cerebral invigoration. Novice and veteran consumers alike enjoy the level effects of Blue Dream, which ease you gently into a calm euphoria. </p>
         <div className='effects-box'>
           <div className='effects-column'>
             <img className='effect-img' src="/assets/Happy.svg"></img>
-            <p className='strain-effect-tag positive-effect'>relaxed</p>
-            <p className='strain-effect-tag positive-effect'>happy</p>
+            {posEffects}
           </div>
           <div className='effects-column'>
             <img className='effect-img' src="/assets/Sad.svg"></img>
-            <p className='strain-effect-tag negative-effect'>dry mouth</p>
-            <p className='strain-effect-tag negative-effect'>see ghosts</p>
+            {negEffects}
           </div>
         </div>
       </div>
